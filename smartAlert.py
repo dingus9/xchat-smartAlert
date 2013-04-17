49bd05ecddfe3110b1986ea840ecc038311a79aa
Nick Shobe <nickshobe@gmail.com>
2013-04-17 10:52:01 -0500

    Finally read for rule creation, most ligic working

--- smartAlert.py
+++ smartAlert.py
@@ -7,7 +7,7 @@ import re
 import time
 
 __module_name__ = "Smart Alerts"
-__module_version__ = "1.0"
+__module_version__ = "1.1"
 __module_description__ = "Customize XChat Alerting + Logging"
 
 
@@ -15,14 +15,16 @@ __module_description__ = "Customize XChat Alerting + Logging"
 class Rules(dict):
 
     def __init__(self):
-        self.rules = [{'users': ['nebnotify'],
+        self.rules = ({'users': ['nebnotify'],
                       'str': 'HMDB ETL',
                       'bool': 'and',
                       'callback': self._hosting_matrix_etl,
-                      'eat':xchat.EAT_NONE},
+                      'eat': xchat.EAT_NONE},
                       {'users': ['meow_bot'],
-                      'callback': self._maas_alert_bot}
-                      ]
+                      'callback': self._maas_alert_bot},
+                      {'users': ['jnott', 'tildedave'],
+                      'callback': self._anymessage},
+                      )
 
         self.alerts = {'maas_issue': {'timeout': 120}}
 
@@ -33,14 +35,15 @@ class Rules(dict):
             return 'none'
 
     def filter_match(self, user, channel, message):
+        retval = False
         for rule in self.rules:
             rslt = {}
             for name, test in rule.iteritems():
                 # Is one of users
                 if name == 'users':
-                    rslt['users'] = False
-                    for user in test:
-                        if xchat.nickcmp(user, test) == 0:
+                    rslt.setdefault('users', False)
+                    for usr in test:
+                        if xchat.nickcmp(usr, user) == 0 or usr == '*':
                             rslt['users'] = True
                             continue
                 elif name == 'str':
@@ -54,15 +57,19 @@ class Rules(dict):
                     else:
                         rslt['channel'] = False
 
-            if 'bool' in self.rules:
-                if rules['bool'] == 'or' and any(rslt.values()):
-                    return rule
+            if 'bool' in rule:
+                if rule['bool'] == 'or' and any(rslt.values()):
+                    print rule
+                    retval = rule
             elif all(rslt.values()):
-                return rule
-            else:
-                return -1
+                retval = rule
 
-    def _hosting_matrix_etl(self, context, user, channel, string):
+        if retval:
+            return retval
+        else:
+            return None
+
+    def _hosting_matrix_etl(self, scope, user, channel, string):
         reg = re.compile('(.*) - (SUCCESS|INFO|ALERT|.*) - - HMDB ETL \((.*)\) (.*)')
         match = reg.match(string)
         groups = match.groups()
@@ -71,12 +78,16 @@ class Rules(dict):
                   'title': 'something'}
         return result
 
-    def _maas_alert_bot(self, context, user, channel, string):
+    def _maas_alert_bot(self, scope, user, channel, string):
         if self._alert_seen('maas_issue'):
             #format alert
             pass
         else:
             return False
+
+    def _anymessage(self, scope, user, channel, string):
+        result = channel + " >> " + user + ': ' + string
+        return result
 
     # Checks for timeout, returns true if timeout met or false if timeout still in effect.
     def _alert_seen(self, name):
@@ -101,10 +112,8 @@ class SmartAlert():
         self.rules = rules
         self.alerters = []
         self.os = platform.system()
-        if self.os == 'Darwin':
-            #self.icon = os.path.abspath(os.path.dirname(pid.exe) +
-            #                            '/../Resources/xchat.icns')
-            self.icon = "/Users/will5324/Desktop/xchat.png"
+        if self.os == 'Darwin': # Handle special osx container setup
+            self.icon = os.getenv("HOME") + "/xchat.png"
         else:
             self.icon = None
 
@@ -119,16 +128,32 @@ class SmartAlert():
         self.alerters.append(alerter)
         alerter.alert(self.notifs[0], self.__class__.__name__, 'Initializing alerter')
 
+    # Message is the main callback for a generic message event.
+    # it assumes no command was issued so it should only be bound to a message even type.
     def message(self, word, word_eol, userdata):
-        user = word[1]
-        message = word_eol[2]
-        channel = xchat.get_list('channel')
-        context = channel.context
-        matched = self.rules.filter_match(user, channel.channel, message)
-        if matched > 0:
-            if 'callback' in self.rules[matched].keys():
-                notify = self.rules[matched]['callback'](context, user, channel, message)
+        # user and message detection is arbitrary to xchat... might want to look at
+        # a more dynamic IRC formmating method for determining the message user and
+        # other attributes.
+        if len(word) == 2:
+            user = word[0]
+            message = word[1]
+        elif len(word) == 3 and word[2] == '@':
+            user = word[0]
+            message = word[1]
+        channel = xchat.get_info('channel')
+        scope = xchat.get_info('server')
+
+        notify = None
+        # Match on one rule for now, later we might support multi matches for multi dests
+        # or what ever. But right now I'm only needing growl and one type of match.
+        matched = self.rules.filter_match(user, channel, message)
+        if matched:
+            if 'callback' in matched:
+                notify = matched['callback'](scope, user, channel, message)
+                if not type(notify) == dict:
+                    notify = {"message": notify, 'title': 'Misc', 'notif': 'Notify'}
             else:
+                #simulated response object... Used if I don't care to callback
                 notify = {'message': message, 'title': 'Misc', 'notif': 'Notify'}
 
         if notify:
@@ -148,9 +173,7 @@ class Growlify():
 
     #send tuple of notifs looking like ["Nice Notifs Name","Other Nice Name"]
     def __init__(self, notifs, appname, icon):
-        image = open('/Users/will5324/test.txt', 'r')
-        #image = open(icon, 'r')
-
+        image = open(icon, 'rb').read()
         self.appname = appname
         self.notifs = notifs
         self.icon = icon
@@ -163,14 +186,15 @@ class Growlify():
             # password = "abc123" # Defaults to a blank password
         self.growl.register()
 
-    def alert(self, notif, title, message):
-        #image = open(self.icon, 'rb').read()
-        print self.icon.replace(' ', '\ ')
+    def alert(self, notif, title, message, icon=None):
+        if icon:
+            icon = open(self.icon, 'rb').read()
         self.growl.notify(
             noteType=notif,
             title=title,
             description=message,
             sticky=False,
+            icon=icon,
             priority=1,)
 
 # Initialize Alert Object
@@ -181,5 +205,12 @@ alerts = SmartAlert(rules)
 
 # Hook Print Events
 print __module_name__ + ": Loading hooks" + sufix
-xchat.hook_print('Generic Message', alerts.message, userdata=None, priority=xchat.PRI_NORM)
+# Possible Event binds:
+# Your Message
+# Message Send -- when sending a priv message
+# Private Message
+# Private Message to Dialog
+# Generic Message -- doesn't seem to work -- probably only at server level
+# Channel Message
+xchat.hook_print('Channel Message', alerts.message, userdata=None, priority=xchat.PRI_NORM)
 xchat.hook_unload(alerts.unload)
