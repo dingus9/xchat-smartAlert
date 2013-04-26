a65788fa818e0c756af159b2e052d4db35d70658
Nick Shobe <nickshobe@gmail.com>
2013-04-26 04:42:51 -0500

    Better support for joining lots of chans... unload and reload hooks

--- smartAlert.py
+++ smartAlert.py
@@ -5,6 +5,7 @@ import platform
 import psutil
 import re
 import time
+import threading
 
 __module_name__ = "smartAlert"
 __module_version__ = "1.1"
@@ -15,20 +16,34 @@ __module_description__ = "Customize XChat Alerting + Logging"
 class Rules(dict):
 
     def __init__(self):
-        self.rules = ({'users': ['nebnotify'],
+
+        self.chain = {'end': 0,
+                      'cont': 1,
+                      'abort': 2}
+
+        self.rules = ({'mention': True,
+                      'callback': self._mention},
+                      {'channels': ['**private'],
+                       'callback': self._mention},
+                      {'channels': ['cloudoutage'],
+                      'callback': self._important},
+                      {'users': ['nebnotify'],
                       'str': 'HMDB ETL',
                       'bool': 'and',
                       'callback': self._hosting_matrix_etl,
                       'eat': xchat.EAT_NONE},
-                      {'users': ['meow_bot'],
+                      {'users': ['meow_bot', 'maas-bot'],
                       'callback': self._maas_alert_bot},
-                      {'channels': ['#ubuntu', '#puppet', '#asd'],
-                      'callback': self._anymessage},
+                      {'channels': ['#nebopsadmins', '#nebops', '#NebOps'],
+                      'callback': self._nebops_activity},
                       )
 
         # used for alert timout queues in callback methods
         self.alerts = {'maas_issue': {'timeout': 120},
-                       'nicks_user': {'timeout': 30}}
+                       'nicks_user': {'timeout': 30},
+                       'nebops_alert': {'timeout': 60},
+                       'flood_threshhold': {'flood_limit': 10, 'flood_persec': 1,
+                                            'timeout': 10}}
 
     def eat(self, rule):
         if 'eat' in self.rules[rule].keys():
@@ -36,7 +51,7 @@ class Rules(dict):
         else:
             return 'none'
 
-    def filter_match(self, user, channel, message):
+    def filter_match(self, user, channel, message, nick):
         retval = False
         for rule in self.rules:
             rslt = {}
@@ -49,55 +64,88 @@ class Rules(dict):
                             rslt['users'] = True
                             continue
                 elif name == 'str':
-                    if message.find(test) > 0:
+                    if message.find(test) > -1:
                         rslt['str'] = True
                     else:
                         rslt['str'] = False
-                elif name == 'channel':
-                    if xchat.nickcmp(channel, test) == 0:
-                        rslt['channel'] = True
-                    else:
-                        rslt['channel'] = False
+                elif name == 'channels':
+                    rslt.setdefault('channels', False)
+                    for chan in test:
+                        if xchat.nickcmp(chan, channel) == 0:
+                            rslt['channels'] = True
+                            continue
+                elif name == 'mention':
+                    rslt['mention'] = False
+                    if message.find(nick) > -1:
+                        rslt['mention'] = True
+                        continue
 
             if 'bool' in rule:
                 if rule['bool'] == 'or' and any(rslt.values()):
-                    print rule
                     retval = rule
+                    break
             elif all(rslt.values()):
                 retval = rule
+                break
 
         if retval:
             return retval
         else:
             return None
 
-    def _test(self, scope, user, channel, string):
-        if self._alert_timeout('nicks_user'):
-            message = string
-            title = "You"
-            return {'notif': 'Notify', 'message': message, 'title': title}
+    def _mention(self, scope, user, channel, string, nick):
+        message = string
+        title = {'**private': "PM: " + user}.get(channel, "Mention: " + channel)
+        return {'notif': 'Message', 'message': user + ': ' + message, 'title': title}
 
-    def _hosting_matrix_etl(self, scope, user, channel, string):
+    def _important(self, scope, user, channel, string, nick):
+        message = user + ': ' + string
+        title = 'Important on: ' + channel
+        return {'notif': 'Message', 'mesage': message, 'title': title}
+
+    def _hosting_matrix_etl(self, scope, user, channel, string, nick):
         reg = re.compile('(.*) - (SUCCESS|INFO|ALERT|.*) - - HMDB ETL \((.*)\) (.*)')
         match = reg.match(string)
         groups = match.groups()
-        result = {'notif': 'Cron', 'message': groups[2].upper() + "->ETL: " +
-                  groups[1] + ' - ' + groups[3],
-                  'title': 'something'}
-        return result
+        return {'notif': 'Cron', 'message': groups[2].upper() + "->ETL: " +
+                groups[1] + ' - ' + groups[3],
+                'title': 'something'}
 
-    def _maas_alert_bot(self, scope, user, channel, string):
+    def _maas_alert_bot(self, scope, user, channel, string, nick):
         if self._alert_timeout('maas_issue'):
-            message = string
+            message = None
+            if user == 'meow_bot':
+                reg = re.compile('(.+) (.+) (.+): (.+) (.+) - (.+):([0-9]{3}) .+').match(string)
+                match = reg.match(string)
+                groups = match.groups()
+                if len(groups) > 4:
+                    message = user + ':' + groups[2] + ' ' + groups[3] + ':' + groups[4]\
+                        + ':' + groups[5]
+            elif user == 'maas-bot':
+                groups = re.compile('(.+) (.+) (.+): (.+) (.+) - .+Found:([0-9]{3}) .+')\
+                    .match(string).groups()
+                if len(groups) > 3:
+                    message = user + ':' + groups[2] + ' ' + groups[3] + ':' + groups[4]\
+                        + ':' + groups[5]
             title = "MaaS Alert"
-            return {'notif': 'Alert', 'message': message, 'title': title}
-        else:
-            return False
+            if message:
+                return {'notif': 'Alert', 'message': message, 'title': title}
 
-    def _anymessage(self, scope, user, channel, string):
+        return None
+
+    def _nebops_activity(self, scope, user, channel, string, nick):
+        if self._alert_timeout('nebops_alert'):
+            message = channel + " >> " + user + ': ' + string
+            title = "Neb Activity"
+            return {'notif': 'Message', 'message': message, 'title': title}
+        else:
+            return None
+
+    def _anymessage(self, scope, user, channel, string, nick):
         result = None
-        if self._alert_timeout('nicks_user'):
-            result = channel + " >> " + user + ': ' + string
+        #if self._alert_timeout('nicks_user'):
+        #    result = channel + " >> " + user + ': ' + string
+        result = channel + " >> " + user + ': ' + string
         return result
 
     # Checks for timeout, returns true if timeout met or false if timeout still in effect.
@@ -106,12 +154,27 @@ class Rules(dict):
         now = int(time.time())
         if 'timeout' in self.alerts[name].keys():
             timeout = self.alerts[name]['timeout']
-            seen = self.alerts[name].setdefault('seen', now)
-            if now > seen + timeout:
+            seen = self.alerts[name].get('seen', now)
+            if seen == now or now > seen + timeout:
                 self.alerts[name]['seen'] = now
                 return True
             else:
                 return False
+
+    # Check name for flooding
+    def _alert_flood(self, name):
+        if name in self.alerts and 'flood_limit' in self.alerts[name]:
+            now = int(time.time())
+            count = self.alerts[name].get('count', 0)
+            timeout = self.alerts[name]['flood_persec']
+            seen = self.alerts[name].get('flood_seen', now)
+            if now > seen + timeout and count <= self.alerts[name].get('flood_limit', 10):
+                self.alerts[name]['flood_seen'] = now
+                self.alerts[name]['flood_count'] = 0
+                return True
+            else:
+                self.alerts[name]['flood_count'] += 1
+                return self._alert_timeout(name)
 
 
 class SmartAlert():
@@ -128,10 +191,18 @@ class SmartAlert():
             self.icon = None
 
         # Setup Alerting Environment
-        self.notifs = ('Message', 'Alert', 'Notify')
+        self.notifs = ('Message', 'Alert', 'Notify', 'Cron')
 
         # Register growl alerter
         self.addAlerter(Growlify(self.notifs, self.appname, self.icon))
+
+    def command(self, word, word_eol, userdata):
+        command = word[0]
+        params = word.get(1, None)
+
+        if params == 'hooks':
+            print self.hooked
+        return xchat.EAT_ALL
 
     def addAlerter(self, alerter):
         print "Registering Alerter: " + alerter.__class__.__name__ + "..."
@@ -140,7 +211,7 @@ class SmartAlert():
 
     # Message is the main callback for a generic message event.
     # it assumes no command was issued so it should only be bound to a message even type.
-    def message(self, word, word_eol, userdata):
+    def general_message(self, word, word_eol, userdata):
         # user and message detection is arbitrary to xchat... might want to look at
         # a more dynamic IRC formmating method for determining the message user and
         # other attributes.
@@ -152,14 +223,29 @@ class SmartAlert():
             message = word[1]
         channel = xchat.get_info('channel')
         scope = xchat.get_info('server')
+        self.base_message(user, message, channel, scope)
 
+    def private_message(self, word, word_eol, userdata):
+        if len(word) == 2:
+            user = word[0]
+            message = word[1]
+        elif len(word) == 3 and word[2] == '@':
+            user = word[0]
+            message = word[1]
+        channel = '**private'
+        scope = xchat.get_info('server')
+        self.base_message(user, message, channel, scope)
+
+    # Called once user, message, channel, and scope have been parsed
+    def base_message(self, user, message, channel, scope):
+        nick = xchat.get_info('nick')
         notify = None
         # Match on one rule for now, later we might support multi matches for multi dests
         # or what ever. But right now I'm only needing growl and one type of match.
-        matched = self.rules.filter_match(user, channel, message)
+        matched = self.rules.filter_match(user, channel, message, nick)
         if matched:
             if 'callback' in matched:
-                notify = matched['callback'](scope, user, channel, message)
+                notify = matched['callback'](scope, user, channel, message, nick)
                 if not type(notify) == dict and notify is not None:
                     notify = {"message": notify, 'title': 'Misc', 'notif': 'Notify'}
             else:
@@ -173,6 +259,32 @@ class SmartAlert():
     def alert(self, **args):
         for alerter in self.alerters:
             alerter.alert(args['notif'], args['title'], args['message'])
+
+    def joiner(self, word, word_eol, userdata):
+        nick = xchat.get_info('nick')
+        if word[0].find(nick) > -1:
+            timeout = 3
+            for handler in self.hooked:
+                xchat.unhook(handler)
+            self.hooked = []
+            self._hook_wait = time.time()
+            threading.Timer(timeout, self._xchatHookTest, (timeout, )).run()
+
+    # Used in joiner above
+    def _xchatHookTest(self, timeout):
+        if time.time() >= self._hook_wait + timeout:
+            self.xchatHook()
+
+    def xchatHook(self):
+        self.hooked = [xchat.hook_print('Channel Message', alerts.general_message,
+                                        userdata=None, priority=xchat.PRI_NORM),
+                       xchat.hook_print('Channel Msg Hilight', alerts.general_message,
+                                        userdata=None, priority=xchat.PRI_NORM),
+                       xchat.hook_print('Private Message', alerts.private_message,
+                                        userdata=None, priority=xchat.PRI_NORM),
+                       xchat.hook_print('Private Message to Dialog',
+                                        alerts.private_message, userdata=None,
+                                        priority=xchat.PRI_NORM)]
 
     # Callback for xchat.unload()
     def unload(self, userdata):
@@ -223,5 +335,10 @@ print __module_name__ + ": Loading hooks" + sufix
 # Private Message to Dialog
 # Generic Message -- doesn't seem to work -- probably only at server level
 # Channel Message
-xchat.hook_print('Channel Message', alerts.message, userdata=None, priority=xchat.PRI_NORM)
+
+# add channel hooks on join... also unloads and reloads every join...
+# prevents floods!
+xchat.hook_command('SMRTA', alerts.command, userdata=None, priority=xchat.PRI_HIGHEST)
+xchat.hook_server('JOIN', alerts.joiner, userdata=None, priority=xchat.PRI_HIGHEST)
+alerts.xchatHook()
 xchat.hook_unload(alerts.unload)
